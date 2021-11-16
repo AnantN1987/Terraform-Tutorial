efa74d334a1d6afdef89d71294000bbb010a43b1
AnantN1987 <42520069+AnantN1987@users.noreply.github.com>
2021-11-16 14:18:28 +0530

    Updated default region

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,11 +1,12 @@
 variable "region" {
-  default = "us-east-1"
+  default = "ap-south-1"
 }
 
 variable "ami_id" {
   type = "map"
 
   default = {
+    ap-south-1   = 'ami-06a0b4e3b7eb7a300'
     us-east-1    = "ami-035b3c7efe6d061d5"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
     eu-central-1 = "ami-9787h5h6nsn"
